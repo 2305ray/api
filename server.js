3d9e1b7bebf2151a0a82d5f5c2bae58e8cf17cb9
smtinfo <smtinformatica@prefeitura.sp.gov.br>
2025-05-07 14:27:29 -0300

    crud completo

--- server.js
+++ server.js
@@ -1,21 +1,69 @@
 import express from 'express'
+import { PrismaClient } from './generated/prisma/index.js'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const users = []
 
 //precisa do tipo de rota (http) e endereço (url)
 //criar uma rota
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
 
-    users.push(req.body)
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age,
+        }
+    })
 
     res.status(201).json(req.body)
 })
 
-app.get('/users', (req, res) => {
-    res.json(users)
-}) 
+app.get('/users', async (req, res) => {
+
+    //filtar com query
+    let users = []
+    if (req.query) {
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name
+            }
+        })
+    } else {
+        const users = await prisma.user.findMany()
+    }
+
+    res.status(201).json(users)
+})
 
 app.listen(3000)
+
+app.put('/users/:id', async (req, res) => {
+
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age,
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.delete('/users/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        },
+    })
+
+    res.status(200).json({ message: "usuário deletado com sucesso" })
+})
